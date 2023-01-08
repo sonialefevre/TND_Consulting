6775b01ae87d11d0bf624bde42b17b63574d38de
sonialefevre <sonialefevre.steno@gmail.com>
2023-01-08 15:39:05 +0100

    Cadding comment to comments book feature working

--- TND_Consulting/src/components/CommentsBook/index.jsx
+++ TND_Consulting/src/components/CommentsBook/index.jsx
@@ -1,12 +1,27 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import instance from "../../utils/axios";
-
 import "./style.scss";
 
 const CommentsBook = () => {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [comment, setComment] = useState("");
+
+  const formData = new FormData();
+  formData.append("clientname", name);
+  formData.append("clientcompany", company);
+  formData.append("comment", comment);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form onSubmit happens");
+    try {
+      instance.post("/commentsbook", formData).then((response) => {
+        console.log(response);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -15,20 +30,37 @@ const CommentsBook = () => {
         <form onSubmit={handleSubmit}>
           <div className="formField">
             <label htmlFor="clientname">Your name</label>
-            <input type="text" name="clientname" id="clientname" required />
+            <input
+              type="text"
+              name="clientname"
+              id="clientname"
+              required
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
           </div>
           <div className="formField">
             <label htmlFor="clientcompany">Your company</label>
             <input
               type="text"
               name="clientcompany"
-              id="clientcompnay"
+              id="clientcompany"
               required
+              onChange={(e) => setCompany(e.target.value)}
+              value={company}
             />
           </div>
           <div className="formField">
             <label htmlFor="comment">Your comment</label>
-            <textarea name="comment" id="comment" rows="5" cols="33" required />
+            <textarea
+              name="comment"
+              id="comment"
+              rows="5"
+              cols="33"
+              required
+              onChange={(e) => setComment(e.target.value)}
+              value={comment}
+            />
           </div>
           <button className="buttonBook" type="submit">
             Send
--- TND_Consulting/src/utils/axios.jsx
+++ TND_Consulting/src/utils/axios.jsx
@@ -3,6 +3,10 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "http://localhost:8000",
   credential: true,
+  headers: {
+    "Content-Type": "multipart/form-data",
+    "Access-Control-Allow-Origin": ["http://localhost:8000"],
+  },
 });
 
 export default instance;
